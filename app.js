d4c292cabedd9c3ca2507f9acb6ed989a21fdb63
mkaychuks <sachufusi@gmail.com>
2021-06-04 12:20:05 -0700

    created a mongoDB connection :cloud: :rocket:

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const userRouter = require('./routes/user');
+const mongoose = require('mongoose');
+require('dotenv/config');
 
 const app = express();
 
-// app Middlewares
+// creating a mongoose connection
+mongoose
+  .connect(process.env.DB_CONNECTION, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then((result) =>
+    app.listen(5000, () => {
+      console.log('Server listening on port 5000...!!');
+    })
+  )
+  .catch((err) => console.log(err));
+
+//
 app.use(express.json()); // body-parser sturvs
 
 app.use('/api/v1/', userRouter); // registration of the routers
-
-app.listen(5000, () => {
-  console.log('Server listening on port 5000...');
-});
