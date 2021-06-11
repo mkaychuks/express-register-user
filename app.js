c1f11e7812b6a5c74c1ead2bdd63a7923567837b
mkaychuks <sachufusi@gmail.com>
2021-06-10 18:13:15 -0700

    made minor changes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express'); // import the express module
 const mongoose = require('mongoose'); // import mongoose package
 require('dotenv/config');
+const userRouter = require('./routes/userRouter');
 
 const app = express(); // express init app
 
@@ -9,6 +10,7 @@ mongoose
   .connect(process.env.DB_CONNECTION, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
   })
   .then(() => {
     app.listen(5000, () => {
@@ -16,3 +18,9 @@ mongoose
     });
   })
   .catch((err) => console.log(err));
+
+// Middlewares for the app
+app.use(express.json()); // init json and enable body-parser
+
+// Middleware for the routers
+app.use('/api/v1/', userRouter);
