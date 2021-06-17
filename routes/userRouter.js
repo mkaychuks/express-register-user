4b6b9380b97282210b9010160a610fe33b08864f
mkaychuks <sachufusi@gmail.com>
2021-06-16 20:38:05 -0700

    :cloud: :rocket:

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,8 +1,13 @@
 const { Router } = require('express'); // importing the Router func from express
-const { userList, userRegister } = require('../controllers/userControllers');
+const {
+  userList,
+  userRegister,
+  userLogin,
+} = require('../controllers/userControllers');
 const router = Router(); // init router
 
 router.get('/', userList); // init list of users
 router.post('/signup/', userRegister); // init register user
+router.post('/login/', userLogin); // init login user
 
 module.exports = router;
